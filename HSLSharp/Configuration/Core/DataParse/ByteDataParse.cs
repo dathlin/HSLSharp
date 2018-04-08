6003de335fa57854c73c6b628812e0cf341c302e
dathlin <hsl200909@163.com>
2018-04-08 22:52:04 +0800

    添加库支持，添加基础的配置器支持。

---
+++ HSLSharp/Configuration/Core/DataParse/ByteDataParse.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Opc.Ua;
+
+namespace HSLSharp.Configuration
+{
+    /// <summary>
+    /// 服务器定义的支持的数据类型处理类，bool,byte,short,ushort,int,uint,long,ulong,float,double,string，以及数组的长度
+    /// </summary>
+    public class ByteDataParse
+    {
+        /// <summary>
+        /// 数据位于字节数据的索引，对于bool变量来说，就是按照位的索引
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// 数据值的名称
+        /// </summary>
+        public string ValueName { get; set; }
+
+        /// <summary>
+        /// 类型的代号，详细参见const数据
+        /// </summary>
+        public int TypeCode { get; set; }
+        
+        /// <summary>
+        /// 类型的长度，对于string来说，就是字符串长度，其他的来说，就是数组长度
+        /// </summary>
+        public int TypeLength { get; set; }
+
+
+
+        /// <summary>
+        /// Bool数据类型
+        /// </summary>
+        public const int DataParseBool = 1;
+        /// <summary>
+        /// Byte数据类型
+        /// </summary>
+        public const int DataParseByte = 2;
+        /// <summary>
+        /// short数据类型
+        /// </summary>
+        public const int DataParseInt16 = 3;
+        /// <summary>
+        /// ushort数据类型
+        /// </summary>
+        public const int DataParseUInt16 = 4;
+        /// <summary>
+        /// int数据类型
+        /// </summary>
+        public const int DataParseInt32 = 5;
+        /// <summary>
+        /// uint数据类型
+        /// </summary>
+        public const int DataParseUInt32 = 6;
+        /// <summary>
+        /// long数据类型
+        /// </summary>
+        public const int DataParseInt64 = 7;
+        /// <summary>
+        /// ulong数据类型
+        /// </summary>
+        public const int DataParseUInt64 = 8;
+        /// <summary>
+        /// float数据类型
+        /// </summary>
+        public const int DataParseFloat = 9;
+        /// <summary>
+        /// double数据类型
+        /// </summary>
+        public const int DataParseDouble = 10;
+        /// <summary>
+        /// string数据类型，ASCII编码
+        /// </summary>
+        public const int DataParseStringASCII = 11;
+        /// <summary>
+        /// string数据类型，Unicode编码
+        /// </summary>
+        public const int DataParseStringUnicode = 12;
+        /// <summary>
+        /// string数据类型，UTF8编码
+        /// </summary>
+        public const int DataParseStringUTF8 = 13;
+
+
+    }
+}
---
+++ HSLSharp/Configuration/Core/DataParse/RegularManagement.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HslCommunication.Core;
+using System.IO;
+using HslCommunication.BasicFramework;
+using System.Windows.Forms;
+
+namespace HSLSharp.Configuration
+{
+    /// <summary>
+    /// 控制所有的数据解析规则的加载和存储功能
+    /// </summary>
+    public class RegularManagement
+    {
+        #region Constructor
+
+        /// <summary>
+        /// 默认的无参构造函数
+        /// </summary>
+        public RegularManagement()
+        {
+            dic_reagulars = new Dictionary<string, DataParseRegular>( );
+            dict_lock = new SimpleHybirdLock( );
+
+            numericalOrder = new SoftNumericalOrder( "R", "", 8, Application.StartupPath + "\\regular_id.txt" );
+        }
+
+        #endregion
+
+        #region Private Member
+
+
+        private SimpleHybirdLock dict_lock = null;
+        private string m_PathSave = string.Empty;                               // 文件加载或是存储的路径
+        private Dictionary<string, DataParseRegular> dic_reagulars = null;      // 所有规则文件的内存列表
+        private SoftNumericalOrder numericalOrder = null;                       // 唯一的暗号生成器
+
+
+        #endregion
+        
+        #region Load Configuration
+
+        public void LoadBy()
+        {
+
+        }
+
+
+        #endregion
+        
+        #region Add Regular
+
+        public void AddRegular( string regularDescription, List<ByteDataParse> dataParses )
+        {
+            string name = numericalOrder.GetNumericalOrder( );
+            string filePath = m_PathSave + "\\Regulars\\" + name + ".regular";
+
+            DataParseRegular regular = new DataParseRegular( )
+            {
+                RegularCode = name,
+                RegularDescription = regularDescription,
+                DataParses = dataParses
+            };
+
+
+            dict_lock.Enter( );
+            try
+            {
+                if (dic_reagulars.ContainsKey( name ))
+                {
+                    dic_reagulars.Remove( name );
+                }
+
+                dic_reagulars.Add( name, regular );
+                regular.SaveToFile( );
+
+                dict_lock.Leave( );
+            }
+            catch
+            {
+                dict_lock.Leave( );
+                throw;                          // 保证异常的时候，退出锁并且还原异常
+            }
+        }
+
+
+        #endregion
+
+        #region Delete Regular
+
+        /// <summary>
+        /// 删除一个字节配置器，如果没有则无效
+        /// </summary>
+        /// <param name="regularCode">规则代号</param>
+        public void DeleteRegular(string regularCode)
+        {
+            dict_lock.Enter( );
+            try
+            {
+                if (dic_reagulars.ContainsKey( regularCode ))
+                {
+                    File.Delete( dic_reagulars[regularCode].FileSavePath );
+                    dic_reagulars.Remove( regularCode );
+                }
+
+                dict_lock.Leave( );
+            }
+            catch
+            {
+                dict_lock.Leave( );
+                throw;                          // 保证异常的时候，退出锁并且还原异常
+            }
+
+        }
+
+        #endregion
+
+        #region Update Regular
+
+        /// <summary>
+        /// 更新字节配置器的信息
+        /// </summary>
+        /// <param name="regularCode">规则代号</param>
+        /// <param name="regularName">规则名称</param>
+        public void UpdateReagular(string regularCode, string regularDescription )
+        {
+            dict_lock.Enter( );
+
+            try
+            {
+                if (dic_reagulars.ContainsKey( regularCode ))
+                {
+                    dic_reagulars[regularCode].RegularDescription = regularDescription;
+                    dic_reagulars[regularCode].SaveToFile( );
+                }
+
+                dict_lock.Leave( );
+            }
+            catch
+            {
+                dict_lock.Leave( );
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 更新字节配置器的信息
+        /// </summary>
+        /// <param name="regularCode">规则代号</param>
+        /// <param name="dataParses">配置规则</param>
+        public void UpdateReagular( string regularCode, List<ByteDataParse> dataParses )
+        {
+            dict_lock.Enter( );
+
+            if (dataParses == null) dataParses = new List<ByteDataParse>( );
+
+            try
+            {
+                if (dic_reagulars.ContainsKey( regularCode ))
+                {
+                    dic_reagulars[regularCode].DataParses = dataParses;
+                    dic_reagulars[regularCode].SaveToFile( );
+                }
+
+                dict_lock.Leave( );
+            }
+            catch
+            {
+                dict_lock.Leave( );
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 更新规则的基础信息，包含了规则名称，规则配置器
+        /// </summary>
+        /// <param name="regularCode">规则代号</param>
+        /// <param name="regularDescription">规则名称</param>
+        /// <param name="dataParses">规则解析列表</param>
+        public void UpdateReagular( string regularCode, string regularDescription, List<ByteDataParse> dataParses )
+        {
+            dict_lock.Enter( );
+
+            try
+            {
+                if (dic_reagulars.ContainsKey( regularCode ))
+                {
+                    dic_reagulars[regularCode].RegularDescription = regularDescription;
+                    if (dataParses == null) dataParses = new List<ByteDataParse>( );
+                    dic_reagulars[regularCode].DataParses = dataParses;
+                    dic_reagulars[regularCode].SaveToFile( );
+                }
+
+                dict_lock.Leave( );
+            }
+            catch
+            {
+                dict_lock.Leave( );
+                throw;
+            }
+        }
+
+
+        #endregion
+
+        #region Get Regulars
+
+
+
+
+        #endregion
+
+    }
+}
